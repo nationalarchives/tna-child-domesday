17b24d46067f29e66be3b8276deeefd0268032a1
Punal Chotrani <punalsc@gmail.com>
2017-04-05 12:09:01 +0100

    grunt tasks setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,46 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        jasmine: {
-            pivotal: {
-                src: 'js/compiled/*.js',
+        // Sass configuration
+        sass: {
+            dist: {
                 options: {
-                    specs: 'js/spec/*Spec.js',
-                    helpers: 'js/spec/*Helper.js'
+                    style: 'compressed'
+                },
+                files: {
+                    'css/styles.css': 'sass/styles.scss'
                 }
+            }
+        },
+        uglify: {
+            options: {
+                banner: ''
+            },
+            target: {
+                src: ['js/main.js'],
+                dest: 'js/main.min.js'
+            }
+        },
+        // Grunt watch tasks
+        watch: {
+            sass: {
+                files: ['sass/*.scss'],
+                tasks: ['sass'],
+            },
+            uglify: {
+                files: ['js/main.js'],
+                tasks: ['uglify']
             }
         }
     });
 
     // Load the plugins that provide the tasks.
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['watch']);
 
 };
---
+++ js/main.js
@@ -0,0 +1,4 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready 1,2,3!" );
+});
